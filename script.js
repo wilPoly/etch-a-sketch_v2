4af50e61b6549d95936a5b70193e112a54766b41
wilpoly <misterwrinkle@gmail.com>
2021-08-31 22:39:02 +0200

    Add clearGrid

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-function generateGrid(size = 16){
-	let cellNumber = size * size;
-	let container = document.querySelector(".container");
-	container.style = `grid-template-columns: repeat(${size}, auto)`;
-   for (let i=0; i < cellNumber; i++) {
-	  let cell = document.createElement("div");
-	  cell.className = "cell";
-	  cell.textContent = i;
-	  container.appendChild(cell);
+const MAX_SIZE = 100;
+const container = document.querySelector(".container");
+const clearButton = document.querySelector("#clearButton");
+
+container.addEventListener("mouseover", event => {
+	if (event.target.className === "cell"){
+		event.target.classList.add("drawn");
 	}
+});
+
+clearButton.addEventListener("click", newGrid);
+
+function generateGrid(size = 16){
+	if (size > 100) size = MAX_SIZE;
+	let cellNumber = size * size;
+	container.style = `grid-template-columns: repeat(${size}, auto)`;
+	for (let i=0; i < cellNumber; i++) {
+		let cell = document.createElement("div");
+		cell.className = "cell";
+		container.appendChild(cell);
+		}
 	console.log(size);
-  }
-  
-  generateGrid();
-// Set up a “hover” effect so that the grid divs change color when your mouse passes over them
+}
+
+function newGrid() {
+	let size = getNewGridSize("Input a number");
+	console.log(typeof size);
+	clearGrid();
+	generateGrid(size);
+}
+
+function getNewGridSize(message) {
+	let size = prompt("What size ?", message);
+	if (size === message) {
+		getNewGridSize(message);
+	}
+	else return Number(size);
+}
+
+function clearGrid() {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
+
+generateGrid();
